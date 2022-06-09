85646eb63a65a157b6afe8b944f95c7c9bf6889e
João Paulo <joaojpmoreira25@gmail.com>
2022-06-09 11:15:29 +0000

    config mongoose

--- admin.js
+++ admin.js
@@ -1,18 +1,49 @@
+//Database
+const mongoose = require('mongoose')
+
+const ProjectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    completed: Boolean,
+    created_at: { type: Date, default: Date.now }
+})
+
+const Project = mongoose.model("Project", ProjectSchema)
+
 //Admin Bro
 const AdminBro = require('admin-bro')
 const AdminBroExpress = require('@admin-bro/express')
+const AdminBroMongoose = require('@admin-bro/mongoose')
 
-const adminBro = new AdminBro({
-    databases: [],
+// use mongoose in AdminBro
+AdminBro.registerAdapter(AdminBroMongoose)
+
+// config
+const adminBroOptions = new AdminBro({
+    resources: [Project],
     rootPath: '/admin',
 })
 
-const router = AdminBroExpress.buildRouter(adminBro)
+const router = AdminBroExpress.buildRouter(adminBroOptions)
 
 //Server
 const express = require('express')
 const server = express()
 
 server 
-    .use(adminBro.options.rootPath, router)
-    .listen(5500, () => console.log("Server started"))
+    .use(adminBroOptions.options.rootPath, router)
+
+// Run App
+const run = async () => {
+    await mongoose.connect("mongodb://localhost/adminbroapp", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    await server.listen(5500, () => console.log("Server started"))
+}
+
+run()
